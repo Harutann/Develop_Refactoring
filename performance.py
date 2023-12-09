64f85b6e0388a29694cab68f8bb7f2cfa53da741
Harutann <harutaikou02231127@gmail.com>
2023-12-09 17:30:57 +0900

    test

--- performance.py
+++ performance.py
@@ -7,12 +7,6 @@ def print_random1():
         if temp in base_list:
             print(f'temp:{temp} in a')
 
-def print_random2():
-    base_list = {randint(0, 100000000) for i in range(1000000)}
-    checked_list = [randint(0, 100000000) for i in range(100)]
-    for temp in checked_list:
-        if temp in base_list:
-            print(f'temp:{temp} in b')
 
 if __name__ == '__main__':
     print_random1()
