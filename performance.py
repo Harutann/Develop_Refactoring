ba1d86ba46785d03aa6463b2cb3009c12735561f
Harutann <harutaikou02231127@gmail.com>
2023-12-09 17:29:32 +0900

    test

---
+++ performance.py
@@ -0,0 +1,18 @@
+from random import randint
+
+def print_random1():
+    base_list = [randint(0, 100000000) for i in range(1000000)]
+    checked_list = [randint(0, 100000000) for i in range(100)]
+    for temp in checked_list:
+        if temp in base_list:
+            print(f'temp:{temp} in a')
+
+def print_random2():
+    base_list = {randint(0, 100000000) for i in range(1000000)}
+    checked_list = [randint(0, 100000000) for i in range(100)]
+    for temp in checked_list:
+        if temp in base_list:
+            print(f'temp:{temp} in b')
+
+if __name__ == '__main__':
+    print_random1()
